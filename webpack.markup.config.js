4d930af5f378e9decedba9b84f40c5750712c0c9
Ivan <iqsk81ad@gmail.com>
2016-10-10 12:53:18 +0300

    #refactor webpack.conf

--- webpack.markup.config.js
+++ webpack.markup.config.js
@@ -2,7 +2,6 @@ const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const htmlTemplate = require('html-webpack-template');
 
-const nodePath = '';
 const path = `${__dirname}/../../`;
 const config = require(`${path}reactMarkup.json`);
 
@@ -24,28 +23,28 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: `${nodePath}eslint-loader`
+        loader: 'eslint-loader'
       },
     ],
     loaders: [
       {
         test: /\.js?$/,
         exclude: /node_modules/,
-        loader: `${nodePath}react-hot-loader!${nodePath}babel-loader`
+        loader: 'react-hot-loader!babel-loader'
       },
       {
         test: /\.scss?$/,
-        loader: `${nodePath}style-loader!${nodePath}css-loader?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!${nodePath}postcss-loader!${nodePath}sass-loader?outputStyle=expanded&sourceMap`
+        loader: 'style-loader!css-loader?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!postcss-loader!sass-loader?outputStyle=expanded&sourceMap'
       },
-      { test: /\.css$/, loader: `${nodePath}style-loader!css-loader` },
-      { test: /\.json$/, loader: `${nodePath}json-loader` },
-      { test: /\.ttf$/, loader: `${nodePath}url-loader?limit=100000&mimetype=application/font-ttf` },
-      { test: /\.eot$/, loader: `${nodePath}url-loader?limit=100000&mimetype=application/vnd.ms-fontobject` },
-      { test: /\.woff2$/, loader: `${nodePath}url-loader?limit=100000&mimetype=application/font-woff2` },
-      { test: /\.woff$/, loader: `${nodePath}url-loader?limit=100000&mimetype=application/font-woff` },
-      { test: /\.png$/, loader: `${nodePath}url-loader?limit=100000000&mimetype=image/png` },
-      { test: /\.jpg$/, loader: `${nodePath}url-loader?limit=100000000&mimetype=image/jpg` },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: `${nodePath}url-loader?limit=10000&mimetype=image/svg+xml` }
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.ttf$/, loader: 'url-loader?limit=100000&mimetype=application/font-ttf' },
+      { test: /\.eot$/, loader: 'url-loader?limit=100000&mimetype=application/vnd.ms-fontobject' },
+      { test: /\.woff2$/, loader: 'url-loader?limit=100000&mimetype=application/font-woff2' },
+      { test: /\.woff$/, loader: 'url-loader?limit=100000&mimetype=application/font-woff' },
+      { test: /\.png$/, loader: 'url-loader?limit=100000000&mimetype=image/png' },
+      { test: /\.jpg$/, loader: 'url-loader?limit=100000000&mimetype=image/jpg' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }
     ]
   },
   resolve: {
